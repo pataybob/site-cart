3c4b51e91e65a54e67aa0a15ce51f47d3119084c
Denis Vishnyackov <Vishka2005@yandex.ru>
2024-04-10 03:14:34 +0400

    index.html upd

--- src/components/burger/burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
-});
